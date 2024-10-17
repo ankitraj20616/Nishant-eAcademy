6b53f27bcd3d14ff2331abab8626e5678b278201
ankitraj20616 <ankitrajthakur803@gmail.com>
2024-10-17 15:53:29 +0530

    some

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,101 +1,99 @@
-$(document).ready(function(){
-
-  $("#loginID").click(function(){
-    $(".error").text('')
+$(document).on("load", function () {
+  $("#loginID").on("click", function (e) {
+    e.preventDefault();
+    $(".error").text("");
     $("#loginModal").modal();
   });
 
-  $("#signUp").click(function(){
-    $(".error").text('')
+  $("#signUp").on("click", function (e) {
+    $(".error").text("");
     $("#signUpModal").modal();
   });
 
-
-  $("#signUpBtn").click(function(e){
+  $("#signUpBtn").on("click", function () {
     e.preventDefault();
-    if (validateSignUpInputs() == false) {
-
-
-    }
+    console.log(this);
+    // if (validateSignUpInputs() == false) {
+    // }
   });
 
-  $("#loginBtn").click(function(e){
+  $("#loginBtn").on("click", function (e) {
     e.preventDefault();
     if (validateLoginInputs() == false) {
-
-
     }
   });
 
-  $("#verify").click(function(e){
+  $("#verify").on("click", function (e) {
     e.preventDefault();
     if (validateOTPInputs() == false) {
-
-
     }
   });
 });
 
-function validateOTPInputs(){
-    var errorFlag = false
+function validateOTPInputs() {
+  var errorFlag = false;
 
-    var emailValue = $("#opt").val();
-    if(emailValue === '') {
-        $("#opt").next('.error').text("OPT is required.")
-        errorFlag = true
-    }
-    return errorFlag
+  var emailValue = $("#opt").val();
+  if (emailValue === "") {
+    $("#opt").next(".error").text("OPT is required.");
+    errorFlag = true;
+  }
+  return errorFlag;
 }
 
-function validateLoginInputs(){
-    var errorFlag = false
+function validateLoginInputs() {
+  var errorFlag = false;
 
-    var emailValue = $("#loginEmail").val();
-    if(emailValue === '') {
-        $("#loginEmail").next('.error').text("Email ID is required.")
-        errorFlag = true
-    }
-    else if (IsEmail(emailValue) === false) {
-        $("#loginEmail").next('.error').text("Entered Email is not Valid!!");
-        errorFlag = true
-    }
-    return errorFlag
+  var emailValue = $("#loginEmail").val();
+  if (emailValue === "") {
+    $("#loginEmail").next(".error").text("Email ID is required.");
+    errorFlag = true;
+  } else if (IsEmail(emailValue) === false) {
+    $("#loginEmail").next(".error").text("Entered Email is not Valid!!");
+    errorFlag = true;
+  }
+  return errorFlag;
 }
 
-function validateSignUpInputs(){
-    var errorFlag = false
+function validateSignUpInputs() {
+  var errorFlag = false;
 
-    var emailValue = $("#inputEmail").val();
-    if(emailValue === '') {
-        $("#inputEmail").next('.error').text("Email ID is required.")
-        errorFlag = true
-    }
-    else if (IsEmail(emailValue) === false) {
-        $("#inputEmail").next('.error').text("Entered Email is not Valid!!");
-        errorFlag = true
-    }
+  var inputFirstName = $("#inputFirstName").val();
+  if (inputFirstName === "") {
+    $("#inputFirstName").next(".error").text("First Name is required.");
+    errorFlag = true;
+  }
 
-    var passwordValue = $("#inputPassword").val();
-    if(passwordValue === '') {
-        $("#inputPassword").next('.error').text("Password is required.")
-        errorFlag = true
-    }
+  var inputLastName = $("#inputLastName").val();
+  if (inputLastName === "") {
+    $("#inputLastName").next(".error").text("Last Name is required.");
+    errorFlag = true;
+  }
 
-    var phoneValue = $("#phnNo").val();
-    if(phoneValue === '') {
-        $("#phnNo").next('.error').text("Phone No is required.")
-        errorFlag = true
-    }
+  var passwordValue = $("#inputPassword").val();
+  if (passwordValue === "") {
+    $("#inputPassword").next(".error").text("Password is required.");
+    errorFlag = true;
+  }
 
-    return errorFlag
-}
+  var confirmPassword = $("#inputConfirmPassword").val();
+  if (confirmPassword === "") {
+    $("#inputConfirmPassword")
+      .next(".error")
+      .text("Confirm password is required.");
+    errorFlag = true;
+  } else if (confirmPassword !== passwordValue) {
+    $("#inputConfirmPassword")
+      .next(".error")
+      .text("Password and Confirm must match!");
+    errorFlag = true;
+  }
 
-function IsEmail(email) {
-    const regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    if (!regex.test(email)) {
-        return false;
-    }
-    else {
-        return true;
-    }
+  var phoneValue = $("#phnNo").val();
+  if (phoneValue === "") {
+    $("#phnNo").next(".error").text("Phone No is required.");
+    errorFlag = true;
+  }
+
+  return false;
 }
